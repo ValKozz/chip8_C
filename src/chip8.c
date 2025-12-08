021500ac9422609398a45a248cb112f2f9b732e7
valkozz <v.kozarev98@gmail.com>
2025-12-08 17:44:03 +0200

    Zero initialized memory and fixed a mistake in checking if the PC has left the system RAM

--- src/chip8.c
+++ src/chip8.c
@@ -7,15 +7,38 @@
 #define PC_START 0x200
 #define MEM_END  0xFFF
 
+uint8_t fonts[] = {
+	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+};
+
 int init(chip8_t *chip8, char *rom_path) {
 	if (chip8 == NULL || rom_path == NULL) {
 		return 1;
 	}
-
+	// init the display
 	display_t displ = displ_init();
 	if (displ == NULL) return 1;
 
 	chip8->displ = displ;
+	// zero out the memory
+	memset(chip8->memory, 0, sizeof(SYS_MEMORY));
+	// copy the fonts into memory
+	memcpy((chip8->memory), fonts, sizeof(fonts));
 
 	FILE *fp = fopen(rom_path, "rb");
 	if (fp == NULL) {
@@ -37,6 +60,7 @@ int init(chip8_t *chip8, char *rom_path) {
 	fread(chip8->memory + PC_START, sizeof(uint8_t), rom_len, fp);
 	chip8->stack.size = 0;
 
+	// put the PC at 0x200
 	chip8->PC = (uint16_t *)(chip8->memory + PC_START);
 	chip8->DT = 0;
 	chip8->ST = 0;
@@ -58,7 +82,7 @@ void decode_and_exec(chip8_t *chip8) {
 	uint16_t instr = *(chip8->PC);
 	printf("INSTR: %04x\n", instr);
 
-	if (chip8->PC == (uint16_t *)chip8->memory + MEM_END) {
+	if ((uint8_t *)chip8->PC == chip8->memory + MEM_END) {
 		printf("Reached MEM_END\n");
 		chip8->running_flag = 0;
 	}
