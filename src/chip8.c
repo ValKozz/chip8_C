ac7186e6e592718177782fcd24919177c50b5c07
valkozz <v.kozarev98@gmail.com>
2025-12-10 13:55:03 +0200

    Added all instructions, minor changes to typos and mistakes

--- src/chip8.c
+++ src/chip8.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 #include "../include/chip8.h"
+#include "../include/input.h"
 #include "SDL2/SDL.h"
 
 #define PC_START 0x200
@@ -62,7 +63,7 @@ static void pop_stack(chip8_t *chip8) {
 	chip8->PC = chip8->stack.array[chip8->stack.size-1];
 	chip8->stack.size--;
 	#ifdef DEBUG
-	printf("Poppef from stack: %d\n", chip8->PC);
+	printf("Popped from stack: %d\n", chip8->PC);
 	#endif
 }
 
@@ -121,8 +122,11 @@ void fetch(chip8_t *chip8) {
 	if (!chip8->paused && !chip8->jmp_flag) {
 		chip8->PC += 2;
 	} 
-	else chip8->jmp_flag = 0; // else set jmp to 0
+	chip8->jmp_flag = 0; // set jmp to 0 if jumped
 
+	#ifdef DEBUG
+	printf("STORED INSTR %04x\n",chip8->opcode);
+	#endif
 	store_instr(chip8);
 }
 
@@ -154,7 +158,7 @@ void decode_and_exec(chip8_t *chip8) {
 
 		else if (instr == 0x00EE) {			
 			#ifdef DEBUG
-			printf("Return to addr %d from stack\n", chip8->stack.array[chip8->stack.size]);
+			printf("Return to addr %d from stack\n", chip8->stack.array[chip8->stack.size - 1]);
 			#endif
 			pop_stack(chip8);
 			}
@@ -174,10 +178,9 @@ void decode_and_exec(chip8_t *chip8) {
 	case 0x10: {
 		uint16_t NNN = instr & 0x0FFF;
 		#ifdef DEBUG
-		printf("JMP to %d.\n", NNN);
+		printf("JMP to %d. ", NNN);
+		printf("INSTR @%d %04x\n", NNN, chip8->memory[NNN]);
 		#endif
-
-		chip8->jmp_flag = 1;
 		
 		if (NNN > SYS_MEMORY) {
 			fprintf(stderr, "ERROR: JMP out of memory! %d\n", NNN);
@@ -185,6 +188,7 @@ void decode_and_exec(chip8_t *chip8) {
 			return;
 		}
 
+		chip8->jmp_flag = 1;
 		chip8->PC = NNN;
 		break;
 	}
@@ -206,294 +210,395 @@ void decode_and_exec(chip8_t *chip8) {
 		chip8->jmp_flag = 1;
 		break;	
 	}
-case 0x30: {
-	uint8_t vx = (instr & 0x0F00) >> 8;
-	uint8_t NN = instr & 0x00FF;
-	
-	if (vx > VF) {
-		fprintf(stderr, "ERROR: Tried to access non-existant register NNN: %d", vx);
-		chip8->running = 0;
-		return;
-	}
-
-	#ifdef DEBUG
-	printf("Compare: %d == %d(NN)\n", 
-		chip8->registers[vx], NN);
-	#endif
-	// Skips the next instruction if VX equals NN
-	if (chip8->registers[vx] == NN) {
-		chip8->PC += 2;
-	}
-	break;
-	}
-case 0x40: {
-	uint8_t vx = (instr & 0x0F00) >> 8;
-	uint8_t NN = instr & 0x00FF;
-
-	if (vx > VF) {
-		fprintf(stderr, "ERROR: Tried to access non-existant register NNN: %d", vx);
-		chip8->running = 0;
-		return;
-	}
-	#ifdef DEBUG
-	printf("Compare NOT: %d != %d(NN)\n", 
-		chip8->registers[vx], NN);
-	#endif
-
-	// Skip next instruction if NN != Vx
-	if (chip8->registers[vx] != NN) {
-		chip8->PC += 2;
-	}  
-	break;
-	}
-case 0x50: {
-	uint8_t vx = (instr & 0x0F00) >> 8;
-	uint8_t vy = (instr & 0X00F0) >> 4;
-	
-	if (vx > VF || vy > VF) {
-		fprintf(stderr, "ERROR: Tried to access non-existant register NNN: vx: %d vy: %d", vx, vy);
-		chip8->running = 0;
-		return;
-	}
-	#ifdef DEBUG
-	printf("Compare: %d == %d(vy)\n", 
-		chip8->registers[vx], chip8->registers[vy]);
-	#endif
-
-	if (chip8->registers[vx] == chip8->registers[vy]) {
-		chip8->PC += 2;
-	}
-
-	break;
-	}
-case 0x60: {
-	uint8_t vx = (instr & 0x0F00) >> 8;
-	uint8_t NN = instr & 0x00FF;
-
-	if (vx > VF) {
-		fprintf(stderr, "ERROR: Tried to access non-existant register NNN: %d", vx);
-		chip8->running = 0;
-		return;
-	}
-	#ifdef DEBUG
-	printf("V%d set to %d == %d(NN)\n", 
-		vx, chip8->registers[vx], NN);
-	#endif
-	chip8->registers[vx] = NN;
-	break;
-	}
-case 0x70: {
-	uint8_t vx = (instr & 0x0F00) >> 8;
-	uint8_t NN = instr & 0x00FF;
-
-	if (vx > VF) {
-		fprintf(stderr, "ERROR: Tried to access non-existant register NNN: %d", vx);
-		chip8->running = 0;
-		return;
-	}
-	#ifdef DEBUG
-	printf("Add %d to V%d = %d\n", 
-		NN, vx, chip8->registers[vx]);
-	#endif
-	chip8->registers[vx] += NN; 
-	break;
-	}
-case 0x80: {
-	uint8_t subflag = instr & 0x000F;
-	uint8_t vx = (instr & 0x0F00) >> 8;
-	uint8_t vy = (instr & 0X00F0) >> 4;
-
-	if (vx > VF || vy > VF) {
-		fprintf(stderr, "ERROR: Tried to access non-existant register NNN: vx: %d vy: %d", vx, vy);
-		chip8->running = 0;
-		return;
-	}
-
-	switch (subflag) {
-		case 0x0:
-			chip8->registers[vx] = chip8->registers[vy];
-			#ifdef DEBUG
-			printf("Set V%d to V%d = %d\n", 
-			vx, vy, chip8->registers[vx]);
-			#endif
-			break;
-		case 0x1:
-			chip8->registers[vx] |= chip8->registers[vy];
-			#ifdef DEBUG
-			printf("bitwise OR V%d to V%d = %d\n", 
-			vx, vy, chip8->registers[vx]);
-			#endif
-			break;
-		case 0x2:
-			chip8->registers[vx] &= chip8->registers[vy];
-			#ifdef DEBUG
-			printf("bitwise AND V%d to V%d = %d\n", 
-			vx, vy, chip8->registers[vx]);
-			#endif
-			break;
-		case 0x3:
-			chip8->registers[vx] ^= chip8->registers[vy];
-			#ifdef DEBUG
-			printf("bitwise XOR V%d to V%d = %d\n", 
-			vx, vy, chip8->registers[vx]);
-			#endif
-			break;
-		case 0x4: {
-			uint16_t res = chip8->registers[vx] + chip8->registers[vy];
-			// max value of a uint8_t 255
-			// check for overflow first
-			if (res > UINT8_MAX) {
-				#ifdef DEBUG
-				printf("overflow detected:  ");
-				#endif		
-				chip8->registers[VF] = 1;
-			}
-			else chip8->registers[VF] = 0;
-			
-			chip8->registers[vx] += chip8->registers[vy];
-			
-			#ifdef DEBUG
-			printf("Add V%d to V%d = %d\n", 
-			vy, vx, chip8->registers[vx]);
-			#endif
-			break;
-			}
-		case 0x5: {
-			// signed to check for underflow
-			int res = chip8->registers[vx] - chip8->registers[vy];
-			// max value of a uint8_t 255
-			// check for overflow first
-			if (res < 0) {
-				#ifdef DEBUG
-				printf("underflow detected: ");
-				#endif		
-				chip8->registers[VF] = 0;
-			}
-			else chip8->registers[VF] = 1;
-			
-			chip8->registers[vx] -= chip8->registers[vy];
-			
-			#ifdef DEBUG
-			printf("substract V%d to V%d = %d\n", 
-			vy, vx, chip8->registers[vx]);
-			#endif
-			break;
-		}
-		case 0x6: {
-			// store least significant bit in VF
-			chip8->registers[VF] = chip8->registers[vx] & 0x1;
-			chip8->registers[vx] >>= 1;
-
-			#ifndef DEBUG
-			printf("Shift V%d >> 1 = %d\n",
-				vx, chip8->registers[vx]);
-			#endif
-			break;
-		}
-		case 0x7: {
-			int res = chip8->registers[vy] - chip8->registers[vx];
-			if (res < 0) {
-				#ifdef DEBUG
-				printf("underflow detected: ");
-				#endif
-				chip8->registers[VF] = 0;
-			}
-			else chip8->registers[VF] = 1;
-			chip8->registers[vx] = chip8->registers[vy] - chip8->registers[vx];
-			#ifdef DEBUG
-			printf("substract V%d(Vy) - V%d = %d\n",
-				vy, vx, chip8->registers[vx]);
-			#endif
-			break;
-		}
-		case 0xE: {
-			// store most significant
-			chip8->registers[VF] = chip8->registers[vx] & (0x01 << 7);
-			chip8->registers[vx] <<= 1;
-			#ifndef DEBUG
-			printf("Shift V%d << 1 = %d\n",
-				vx, chip8->registers[vx]);
-			#endif
-			break;
-		}
-	}
-	break;
-	}
-
-case 0x90: {
-	uint8_t vx = (instr & 0x0F00) >> 8;
-	uint8_t vy = (instr & 0X00F0) >> 4;
+	case 0x30: {
+		uint8_t vx = (instr & 0x0F00) >> 8;
+		uint8_t NN = instr & 0x00FF;
 		
-	if (vx > VF || vy > VF) {
-		fprintf(stderr, "ERROR: Tried to access non-existant register NNN: vx: %d vy: %d", vx, vy);
-		chip8->running = 0;
-		return;
-	}
-
-	#ifndef DEBUG
-	printf("Skip next V%d != V%d\n",
-		vx, vy);
-	#endif
-
-	if (chip8->registers[vx] != chip8->registers[vy]) {
-		chip8->PC +=2;
-	}
-	break;	
-	}
-case 0xA0: {
-	uint16_t NNN = instr & 0X0FFF;
-	chip8->I = NNN;
-	#ifndef DEBUG
-	printf("Set I to %d\n",
-		chip8->I);
-	#endif
-	break;
-	}
-case 0xB0: {
-	uint16_t NNN = instr & 0X0FFF;
-	#ifndef DEBUG
-	printf("JMP to (V0) %d + %d = %d\n",
-		chip8->registers[V0], NNN, NNN + chip8->registers[V0]);
-	#endif
-	
-	chip8->PC = chip8->registers[V0] + NNN;
-	chip8->jmp_flag = 1;
-	break;
-	}
-case 0xD0: {
-	// TODO draw
-	uint8_t vx = (instr & 0x0F00) >> 8;
-	uint8_t vy = (instr & 0X00F0) >> 4;
-	uint8_t N = instr & 0X000F;
-
-	if (vx > VF || vy > VF) {
-		fprintf(stderr, "ERROR: Tried to access non-existant register NNN: vx: %d vy: %d", vx, vy);
-		chip8->running = 0;
-		return;
-	}
-
-	#ifdef DEBUG
-	printf("TODO Draw on screen: %d %d height: %d\n",
-		chip8->registers[vx], chip8->registers[vy], N);
-	#endif
-
-	break;
-	}
-case 0xE0: {
-	uint16_t subflag = instr & 0x00FF;
-	if (subflag == 0x9E) {
-
-	}	
-	else if (subflag == 0xA1) {
-
-	}
-	else {
-		fprintf(stderr, "ERROR: ILLEGAL OPCODE 0xE0: %04x\n", instr);
-		chip8->running = 0;
-		return;
+		if (vx > VF) {
+			fprintf(stderr, "ERROR: Tried to access non-existant register: %d", vx);
+			chip8->running = 0;
+			return;
 		}
+
+		#ifdef DEBUG
+		printf("Compare: %d == %d(NN)\n", 
+			chip8->registers[vx], NN);
+		#endif
+		// Skips the next instruction if VX equals NN
+		if (chip8->registers[vx] == NN) {
+			chip8->PC += 2;
+		}
+		break;
+		}
+	case 0x40: {
+		uint8_t vx = (instr & 0x0F00) >> 8;
+		uint8_t NN = instr & 0x00FF;
+
+		if (vx > VF) {
+			fprintf(stderr, "ERROR: Tried to access non-existant register: %d", vx);
+			chip8->running = 0;
+			return;
+		}
+		#ifdef DEBUG
+		printf("Compare NOT: %d != %d(NN)\n", 
+			chip8->registers[vx], NN);
+		#endif
+
+		// Skip next instruction if NN != Vx
+		if (chip8->registers[vx] != NN) {
+			chip8->PC += 2;
+		}  
+		break;
+		}
+	case 0x50: {
+		uint8_t vx = (instr & 0x0F00) >> 8;
+		uint8_t vy = (instr & 0X00F0) >> 4;
+		
+		if (vx > VF || vy > VF) {
+			fprintf(stderr, "ERROR: Tried to access non-existant register: vx: %d vy: %d", vx, vy);
+			chip8->running = 0;
+			return;
+		}
+		#ifdef DEBUG
+		printf("Compare: %d == %d(vy)\n", 
+			chip8->registers[vx], chip8->registers[vy]);
+		#endif
+
+		if (chip8->registers[vx] == chip8->registers[vy]) {
+			chip8->PC += 2;
+		}
+
+		break;
+		}
+	case 0x60: {
+		uint8_t vx = (instr & 0x0F00) >> 8;
+		uint8_t NN = instr & 0x00FF;
+
+		if (vx > VF) {
+			fprintf(stderr, "ERROR: Tried to access non-existant register: %d", vx);
+			chip8->running = 0;
+			return;
+		}
+		#ifdef DEBUG
+		printf("V%d set to %d = %d(NN)\n", 
+			vx, chip8->registers[vx], NN);
+		#endif
+		chip8->registers[vx] = NN;
+		break;
+		}
+	case 0x70: {
+		uint8_t vx = (instr & 0x0F00) >> 8;
+		uint8_t NN = instr & 0x00FF;
+
+		if (vx > VF) {
+			fprintf(stderr, "ERROR: Tried to access non-existant register: %d", vx);
+			chip8->running = 0;
+			return;
+		}
+		#ifdef DEBUG
+		printf("Add %d to V%d = %d\n", 
+			NN, vx, chip8->registers[vx]);
+		#endif
+		chip8->registers[vx] += NN; 
+		break;
+		}
+	case 0x80: {
+		uint8_t subflag = instr & 0x000F;
+		uint8_t vx = (instr & 0x0F00) >> 8;
+		uint8_t vy = (instr & 0X00F0) >> 4;
+
+		if (vx > VF || vy > VF) {
+			fprintf(stderr, "ERROR: Tried to access non-existant register: vx: %d vy: %d", vx, vy);
+			chip8->running = 0;
+			return;
+		}
+
+		switch (subflag) {
+			case 0x0:
+				chip8->registers[vx] = chip8->registers[vy];
+				#ifdef DEBUG
+				printf("Set V%d to V%d = %d\n", 
+				vx, vy, chip8->registers[vx]);
+				#endif
+				break;
+			case 0x1:
+				chip8->registers[vx] |= chip8->registers[vy];
+				#ifdef DEBUG
+				printf("bitwise OR V%d to V%d = %d\n", 
+				vx, vy, chip8->registers[vx]);
+				#endif
+				break;
+			case 0x2:
+				chip8->registers[vx] &= chip8->registers[vy];
+				#ifdef DEBUG
+				printf("bitwise AND V%d to V%d = %d\n", 
+				vx, vy, chip8->registers[vx]);
+				#endif
+				break;
+			case 0x3:
+				chip8->registers[vx] ^= chip8->registers[vy];
+				#ifdef DEBUG
+				printf("bitwise XOR V%d to V%d = %d\n", 
+				vx, vy, chip8->registers[vx]);
+				#endif
+				break;
+			case 0x4: {
+				uint16_t res = chip8->registers[vx] + chip8->registers[vy];
+				// max value of a uint8_t 255
+				// check for overflow first
+				if (res > UINT8_MAX) {
+					#ifdef DEBUG
+					printf("overflow detected:  ");
+					#endif		
+					chip8->registers[VF] = 1;
+				}
+				else chip8->registers[VF] = 0;
+				
+				chip8->registers[vx] += chip8->registers[vy];
+				
+				#ifdef DEBUG
+				printf("Add V%d to V%d = %d\n", 
+				vy, vx, chip8->registers[vx]);
+				#endif
+				break;
+				}
+			case 0x5: {
+				// signed to check for underflow
+				int res = chip8->registers[vx] - chip8->registers[vy];
+				// max value of a uint8_t 255
+				// check for overflow first
+				if (res < 0) {
+					#ifdef DEBUG
+					printf("underflow detected: ");
+					#endif		
+					chip8->registers[VF] = 0;
+				}
+				else chip8->registers[VF] = 1;
+				
+				chip8->registers[vx] -= chip8->registers[vy];
+				
+				#ifdef DEBUG
+				printf("substract V%d to V%d = %d\n", 
+				vy, vx, chip8->registers[vx]);
+				#endif
+				break;
+			}
+			case 0x6: {
+				// store least significant bit in VF
+				chip8->registers[VF] = chip8->registers[vx] & 0x1;
+				chip8->registers[vx] >>= 1;
+
+				#ifdef DEBUG
+				printf("Shift V%d >> 1 = %d\n",
+					vx, chip8->registers[vx]);
+				#endif
+				break;
+			}
+			case 0x7: {
+				int res = chip8->registers[vy] - chip8->registers[vx];
+				if (res < 0) {
+					#ifdef DEBUG
+					printf("underflow detected: ");
+					#endif
+					chip8->registers[VF] = 0;
+				}
+				else chip8->registers[VF] = 1;
+				chip8->registers[vx] = chip8->registers[vy] - chip8->registers[vx];
+				#ifdef DEBUG
+				printf("substract V%d(Vy) - V%d = %d\n",
+					vy, vx, chip8->registers[vx]);
+				#endif
+				break;
+			}
+			case 0xE: {
+				// store most significant
+				chip8->registers[VF] = chip8->registers[vx] & (0x01 << 7);
+				chip8->registers[vx] <<= 1;
+				#ifdef DEBUG
+				printf("Shift V%d << 1 = %d\n",
+					vx, chip8->registers[vx]);
+				#endif
+				break;
+			}
+		}
+		break;
+		}
+
+	case 0x90: {
+		uint8_t vx = (instr & 0x0F00) >> 8;
+		uint8_t vy = (instr & 0X00F0) >> 4;
+		
+		if (vx > VF || vy > VF) {
+			fprintf(stderr, "ERROR: Tried to access non-existant register NNN: vx: %d vy: %d", vx, vy);
+			chip8->running = 0;
+			return;
+		}
+
+		#ifdef DEBUG
+		printf("Skip next V%d != V%d\n",
+			vx, vy);
+		#endif
+
+		if (chip8->registers[vx] != chip8->registers[vy]) {
+			chip8->PC +=2;
+		}
+		break;	
+		}
+	case 0xA0: {
+		uint16_t NNN = instr & 0X0FFF;
+		chip8->I = NNN;
+		#ifdef DEBUG
+		printf("Set I to %d\n",
+			chip8->I);
+		#endif
+		break;
+		}
+	case 0xB0: {
+		uint16_t NNN = instr & 0X0FFF;
+		#ifdef DEBUG
+		printf("JMP to (V0) %d + %d = %d\n",
+			chip8->registers[V0], NNN, NNN + chip8->registers[V0]);
+		#endif
+		
+		chip8->PC = chip8->registers[V0] + NNN;
+		chip8->jmp_flag = 1;
+		break;
+		}
+	case 0xD0: {
+		// TODO draw
+		uint8_t vx = (instr & 0x0F00) >> 8;
+		uint8_t vy = (instr & 0X00F0) >> 4;
+		uint8_t N = instr & 0X000F;
+
+		if (vx > VF || vy > VF) {
+			fprintf(stderr, "ERROR: Tried to access non-existant register NNN: vx: %d vy: %d", vx, vy);
+			chip8->running = 0;
+			return;
+		}
+
+		#ifdef DEBUG
+		printf("TODO Draw on screen: %d %d height: %d\n",
+			chip8->registers[vx], chip8->registers[vy], N);
+		#endif
+
+		break;
+		}
+	case 0xE0: {
+		uint16_t subflag = instr & 0x00FF;
+		uint8_t vx = (instr & 0x0F00) >> 8;
+
+		if (subflag == 0x9E) {
+			#ifdef DEBUG
+				printf("Skip on key %d\n", chip8->registers[vx]);
+			#endif
+
+			if (chip8->key == chip8->registers[vx]) {
+				chip8->PC += 2;
+			}
+		}	
+		else if (subflag == 0xA1) {
+			if (subflag == 0x9E) {
+				#ifdef DEBUG
+				printf("Skip NOT on key %d\n", chip8->registers[vx]);
+				#endif
+
+				if (chip8->key != chip8->registers[vx]) {
+					chip8->PC += 2;
+				}
+		}
+		else {
+			fprintf(stderr, "ERROR: ILLEGAL OPCODE in 0xE0: %04x\n", instr);
+			chip8->running = 0;
+			return;
+			}
+		}
+		break;
+		}
+	case 0xF0: {
+		uint8_t vx = (instr & 0x0F00) >> 8;
+		uint8_t subflag = instr & 0x00FF;
+
+		if (vx > VF) {
+			fprintf(stderr, "ERROR: Tried to access non-existant register NNN: %d", vx);
+			chip8->running = 0;
+			return;
+		}
+		
+		switch (subflag) {
+		case 0x07:
+			#ifdef DEBUG
+			printf("Set vx to timer %d\n", chip8->DT);
+			#endif
+			chip8->registers[vx] = chip8->DT;
+			break;
+		case 0x0A:
+			// TODO ST and DT should continue
+			// A halt flag should be implemented?
+			#ifdef DEBUG
+			printf("Await input to set V%d to\n", vx);
+			#endif
+
+			while (chip8->key == 0) {
+				handle_input(chip8);
+			}
+
+			chip8->registers[vx] = chip8->key;
+			break;
+		case 0x15:
+			#ifdef DEBUG
+			printf("Set DT to %d\n", chip8->registers[vx]);
+			#endif
+			chip8->DT = chip8->registers[vx];
+			break;
+		case 0x18:
+			#ifdef DEBUG
+			printf("Set ST to %d\n", chip8->registers[vx]);
+			#endif
+			chip8->ST = chip8->registers[vx];
+			break;
+		case 0x1E:
+			#ifdef DEBUG
+			printf("Add %d to I = %d\n", chip8->registers[vx], chip8->I + chip8->registers[vx]);
+			#endif
+			chip8->I = chip8->registers[vx];
+			break;
+		case 0x29:
+			#ifdef DEBUG
+			printf("Set I sprite addr: %d\n", chip8->registers[vx]);
+			#endif
+			chip8->I = chip8->registers[vx] & 0x0F;
+			break;
+		case 0x33:
+			#ifdef DEBUG
+			printf("set BCD OP\n");
+			#endif
+			chip8->memory[chip8->I] = chip8->registers[vx] / 100;
+			chip8->memory[chip8->I + 1] = (chip8->registers[vx] / 10) % 10;
+			chip8->memory[chip8->I + 2] = (chip8->registers[vx] % 100) % 10;
+			break;
+		case 0x55: {
+			size_t i = 0;
+			while (i < VF) {
+				chip8->memory[chip8->I + i] = chip8->registers[i];
+				i++;
+			}
+			break;
+		}
+		case 0x65: {
+			size_t i = 0;
+			while (i < VF) {
+				chip8->registers[i] = chip8->memory[chip8->I + i];
+				i++;
+			}
+			break;
+		}
+		default:
+			fprintf(stderr, "ERROR: ILLEGAL OPCODE in 0xF0");
+			chip8->running = 0;
+			return;
+		}
+		break;
 	}
-	break;
 
 	default:
 		fprintf(stderr, "ERROR: UNKNOWN OPCODE: %04x\n", instr);
@@ -501,8 +606,8 @@ case 0xE0: {
 		return;
 	}
 
-	if (chip8->PC+1 == MEM_END) {
-		fprintf(stderr, "ERROR: Reached MEM_END\n");
+	if (chip8->PC+2 == MEM_END) {
+		fprintf(stderr, "ERROR: PC Reached MEM_END\n");
 		// temp
 		chip8->paused = 1;
 	}
